a7ba56df07fe344a7d748ab444ab85826f1cfdb5
Behdad Abedi <behdad.222@gmail.com>
2017-09-03 21:27:41 +0430

    Remove modules

--- ffmpeg_build.sh
+++ ffmpeg_build.sh
@@ -22,12 +22,6 @@ make clean
 --cpu="$CPU" \
 --enable-runtime-cpudetect \
 --sysroot="$NDK_SYSROOT" \
---enable-pic \
---enable-libx264 \
---enable-libass \
---enable-libfreetype \
---enable-libfribidi \
---enable-fontconfig \
 --enable-pthreads \
 --disable-debug \
 --disable-ffserver \
@@ -44,7 +38,6 @@ make clean
 --prefix="${2}/build/${1}" \
 --extra-cflags="-I${TOOLCHAIN_PREFIX}/include $CFLAGS" \
 --extra-ldflags="-L${TOOLCHAIN_PREFIX}/lib $LDFLAGS" \
---extra-libs="-lpng -lexpat -lm" \
 --extra-cxxflags="$CXX_FLAGS" || exit 1
 
 make -j${NUMBER_OF_CORES} && make install || exit 1
